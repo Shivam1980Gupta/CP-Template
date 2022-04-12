9e3b5b6b06bb0efcfd5f3444a773167c2939525e
Shivam Gupta <shivam1980gupta@gmail.com>
2022-04-12 14:11:57 +0530

    Initial Commit

---
+++ ###CP_TEMPLATE1#####.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+ 
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x)              x.begin(), x.end()
+#define fo(i,a,n,p)         for(int i=a; (p<0 && i>n)||(p>0 && i<n); i+=p)
+#define pb                  push_back
+#define ub                  upper_bound
+#define lb                  lower_bound
+#define ff                  first
+#define ss                  second
+#define int                 long long
+#define dbg(x) cout << #x << " == " << x << "\n";
+#define pb                  push_back
+#define mp                  make_pair
+#define pii                 pair<int,int>
+#define vi                  vector<int>
+#define mii                 map<int,int>
+#define pqb                 priority_queue<int>
+#define pqs                 priority_queue<int,vi,greater<int> >
+#define setbits(x)          __builtin_popcountll(x)
+#define zrobits(x)          __builtin_ctzll(x)
+#define mod                 1000000007
+#define inf                 1e18
+#define ps(x,y)             fixed<<setprecision(y)<<x
+#define mk(arr,n,type)      type *arr=new type[n];
+#define w(x)                int x; cin>>x; while(x--)
+typedef long long ll;
+typedef long double ld;
+// mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+void shivam_Gupta12345(){
+    
+}
+int32_t main()
+{
+    fast;
+    int t=1; 
+    cin>>t;
+    while(t--)
+        shivam_Gupta12345();
+    return 0;
+}
+
+
